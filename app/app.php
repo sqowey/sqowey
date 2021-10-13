f5e0efa9164ee3b4b99eb05ff1325eea747fa158
CuzImBisonratte <CuzImBisonratte@gmail.com>
2021-10-13 21:53:05 +0200

    main page grid (+) chatbox (+) report-file writing (-)

--- app/app.php
+++ app/app.php
@@ -8,37 +8,49 @@
 	}
 ?>
 
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
 
-    <head>
-        <meta charset="utf-8">
-        <title>Home Page</title>
-        <link href="app_style.css" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-    </head>
+<head>
+    <meta charset="utf-8">
+    <title>Home Page</title>
+    <link href="app_style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
 
-    <body>
-        <div id="topbar">
-            <div>
-                <h1>Wudsim</h1>
-                <a href="profile.php"><i class="fas fa-user-circle"></i>Profil</a>
-                <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Ausloggen</a>
+<body>
+    <div class="container">
+        <div class="main">
+            <div id="main">
+                <h2>NOCH IM AUFBAU!</h2>
+                <p>Welcome back,
+                    <?=$_SESSION['name']?>!
+                </p>
             </div>
         </div>
-        <div id="main">
-            <h2>NOCH IM AUFBAU!</h2>
-            <p>Welcome back,
-                <?=$_SESSION['name']?>!</p>
+        <div class="people"></div>
+        <div class="servers"></div>
+        <div class="chat">
+            <div id="input-box">
+                <input placeholder="Nachricht" id="input_field"></input>
+            </div>
         </div>
-        <div id="navigation">
+        <div class="name"></div>
+        <div class="channels"></div>
+        <div class="status">
+            <span id="online"><i class="fas fa-bullseye"></i></span>
+            <span id="away"><i class="fas fa-bullseye"></i></span>
+            <span id="notDisturb"><i class="fas fa-bullseye"></i></span>
+        </div>
+    </div>
+    <!-- <div id="topbar">
+        <div>
+            <h1>Wudsim</h1>
+            <a href="profile.php"><i class="fas fa-user-circle"></i>Profil</a>
+            <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Ausloggen</a>
+        </div>
+    </div>
+    -->
+</body>
 
-        </div>
-        <div id="statussetter">
-            <span id="online">&SmallCircle;</span>
-            <span id="away">&SmallCircle;</span>
-            <span id="notDisturb">&SmallCircle;</span>
-        </div>
-    </body>
-
-    </html>
+</html>
--- app/message_script.js
+++ app/message_script.js
@@ -1,28 +1,28 @@
-function setErrorMessage(){
+function setErrorMessage() {
     errorMessageSub = location.search.substring(1);
     errorMessage = errorMessageSub.split("&")[0];
     errorMessage = errorMessage.split("=")[1];
-    while(errorMessage.includes("%22")){
+    while (errorMessage.includes("%22")) {
         errorMessage = errorMessage.replace("%22", "");
     }
-    while(errorMessage.includes("%20")){
+    while (errorMessage.includes("%20")) {
         errorMessage = errorMessage.replace("%20", " ");
     }
-    
+
     document.getElementById("errorBox").innerHTML = errorMessage;
 }
 setErrorMessage();
 
-function login(){
+function login() {
     location.replace("index.html");
 }
 
-function report(){
-    location.replace("report.html");
+function report() {
+    location.replace("support.html");
 }
 
-function showReportButton(){
-    if(location.search.substring().includes("showReportButton=false")){
+function showReportButton() {
+    if (location.search.substring().includes("showReportButton=false")) {
         document.getElementById("reportButton").style.display = "none";
     }
 }
