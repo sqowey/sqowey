4fc9a81f6beee9053e741ff6f5b626e8a571598c
CuzImBisonratte <cuzimbisonratte@gmail.com>
2021-10-31 22:39:54 +0100

    corrected redirection

--- app/app.php
+++ app/app.php
@@ -52,7 +52,7 @@
         <!-- The Name thats on the top of the page -->
         <div class="name">
             <div id="actions">
-                <i class="fas fa-cog" onclick='location.href = "./settings.html";'></i>
+                <i class="fas fa-cog" onclick='location.href = "./settings.php";'></i>
             </div>
             <span>Test</span>
             <div id="personal_actions">
