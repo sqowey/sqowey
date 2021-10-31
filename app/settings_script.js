2c98db2057285343c55522a14d1ad3ec88700ef6
CuzImBisonratte <cuzimbisonratte@gmail.com>
2021-10-31 23:40:26 +0100

    added commentary

--- app/settings_script.js
+++ app/settings_script.js
@@ -1,10 +1,21 @@
 mail = document.getElementById("account_mail");
 username = document.getElementById("account_username");
 phone = document.getElementById("account_phone");
+
+
+// 
+// Background color changer on value change
+// 
+
+// Get initial values
 old_mail = mail.value;
 old_username = username.value;
 old_phone = phone.value;
+
+// The function that get's called repeatedly
 function accountDetailsColor(){
+
+    // Check if value has changed and if so set new background color
     if(mail.value!=old_mail){
         mail.style.background = "#4e8fc2"
     }else{
@@ -21,4 +32,6 @@ function accountDetailsColor(){
         phone.style.background = "#dee0e4"
     }
 }
+
+// Add an interval-caller for the account function
 setInterval(accountDetailsColor, 10);
