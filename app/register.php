f8993edeb2f8eb5598d2e54599c8d0f8e30ee7f3
CuzImBisonratte <CuzImBisonratte@gmail.com>
2021-11-22 19:03:53 +0100

    added more standart images

--- app/register.php
+++ app/register.php
@@ -1,7 +1,7 @@
 <?php
 
     // Standart image variable
-    $standart_img = "../files/avatars/standart.jpg";
+    $standart_imgs_folder = "../files/avatars/standart/";
 
     // Database credentials
     $DATABASE_HOST = 'localhost';
@@ -119,8 +119,12 @@
             // Username doesnt exists, insert new account
             if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, avatar) VALUES (?, ?, ?, ?)')) {
                 // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+
+                // Choose a random image 
+                $random_img = $standart_imgs_folder.rand(1,8).".png";
+
                 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-                $stmt->bind_param('ssss', $_POST['username'], $password, $_POST['email'], $standart_img);
+                $stmt->bind_param('ssss', $_POST['username'], $password, $_POST['email'], $random_img);
                 $stmt->execute();
                 header('Location: message.html?message=%22Erfolgreich%20registriert%22&showReportButton=false');
                 exit();
