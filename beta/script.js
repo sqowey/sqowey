67ed060f86433e29ef764c68870c74a2620904f1
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-03-03 18:33:17 +0100

    added link menu on beta page

---
+++ beta/script.js
@@ -0,0 +1,9 @@
+function openAdmin() {
+    var admin = document.getElementById('adminContainer');
+    admin.style.display = 'flex';
+}
+
+function closeAdmin() {
+    var admin = document.getElementById('adminContainer');
+    admin.style.display = 'none';
+}
