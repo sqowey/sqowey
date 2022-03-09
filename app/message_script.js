78d4a18d7a8e9e91638bca80072df1e5aa001a52
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-03-09 20:20:51 +0100

    added message codes to javascript

--- app/message_script.js
+++ app/message_script.js
@@ -1,3 +1,20 @@
+// The messages that refer to the codes
+code_messages_en = {
+    "00": "Success",
+    "01": "Error with<br>the Database",
+    "02": "Please fill in<br>all the fields",
+    "04": "No user with that password exists",
+    "05": "No user with that email exists",
+    "06": "No user with that username exists",
+    "07": "Supportticket<br>created",
+    "08": "EMail changed",
+    "09": "Password changed",
+    "10": "Username changed",
+    "11": "Avatar changed"
+};
+
+
+
 // Get the message from the url
 // Its the string between the ?message= and the &
 // If there is no & in the url the message goes to the end of the url
