ed34c38c3541df3e24a68cdcc20447e04cb8fd5f
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-04-20 17:34:07 +0200

    added warning when time is 0

--- app/settings/deleteaccount/script.js
+++ app/settings/deleteaccount/script.js
@@ -3,19 +3,33 @@ function time_changed() {
     // Get the chosen option value
     var time_until_deletion = document.getElementById("time_until_deletion").value;
 
-    // Add the time until deletion to the current time (it's in days)
-    var time_until_deletion_in_mills = time_until_deletion * 24 * 60 * 60 * 1000;
-    var date = new Date();
-    date = date.getTime();
-    date = date + time_until_deletion_in_mills;
+    // If the chosen option value is 0 
+    if (time_until_deletion == 0) {
 
+        // Edit the submit button
+        document.getElementById("submit_button").value = "Account sofort löschen";
 
-    // Create a date string (dd.mm.yyyy)
-    var date_string = new Date(date);
-    date_string = date_string.toLocaleDateString();
+        // Add the warning
+        document.getElementById("warning").innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i>Dein Konto wird sofort gelöscht! Bist du dir wirklich sicher?<i class="fa-solid fa-triangle-exclamation"></i><br><br>';
 
-    // Edit the submit button
-    document.getElementById("submit_button").value = "Account am " + date_string + "" + " löschen.";
+    } else {
+
+        // Add the time until deletion to the current time (it's in days)
+        var time_until_deletion_in_mills = time_until_deletion * 24 * 60 * 60 * 1000;
+        var date = new Date();
+        date = date.getTime();
+        date = date + time_until_deletion_in_mills;
+
+        // Remove the warning
+        document.getElementById("warning").innerHTML = "";
+
+        // Create a date string (dd.mm.yyyy)
+        var date_string = new Date(date);
+        date_string = date_string.toLocaleDateString();
+
+        // Edit the submit button
+        document.getElementById("submit_button").value = "Account am " + date_string + "" + " löschen.";
+    }
 }
 
 time_changed();
