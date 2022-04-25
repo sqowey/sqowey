db49ae930ea8535fa367db2d623fb3533628fe0f
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-04-25 22:49:22 +0200

    changed account details to be a form

--- app/settings/scripts/getaccount.js
+++ app/settings/scripts/getaccount.js
@@ -3,7 +3,7 @@ var avatar_left_top = document.getElementById("avatar_left_top");
 var name_field = document.getElementById("name_field");
 var email_field = document.getElementById("email_field");
 var account_settings_mail = document.getElementById("account_settings_mail");
-var account_settings_name = document.getElementById("account_settings_name");
+var account_settings_name = document.getElementById("account_settings_username");
 var account_settings_phone = document.getElementById("account_settings_phone");
 var account_settings_avatar = document.getElementById("account_settings_avatar");
 
@@ -30,14 +30,14 @@ $.ajax({
 
             // Set the name fields
             name_field.innerHTML = username;
-            account_settings_name.innerHTML = username;
+            account_settings_name.value = username;
 
             // Set the email fields
             email_field.innerHTML = email;
-            account_settings_mail.innerHTML = email;
+            account_settings_mail.value = email;
 
             // Set the phone field
-            account_settings_phone.innerHTML = phone;
+            account_settings_phone.value = phone;
 
             // Set the avatar fields
             avatar_left_top.innerHTML = "<img src='" + avatar + "'>";
