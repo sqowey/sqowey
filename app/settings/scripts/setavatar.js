d098e7715e9b793a610ba76d4d5373cbec23bb52
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-04-27 23:29:16 +0200

    Revert "51 redesign settings"

--- app/settings/scripts/setavatar.js
+++
@@ -1,9 +0,0 @@
-// Add the event listener
-document.getElementById("account_settings_avatar_file").addEventListener("change", function() {
-
-    // Get file
-    var file = this.files[0];
-
-    // Set the avatar preview
-    document.getElementById("account_settings_avatar").src = URL.createObjectURL(file);
-});
