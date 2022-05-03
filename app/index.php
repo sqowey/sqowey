4702e87af4e639a97faf084653a5e1dc92b51611
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-05-03 23:15:12 +0200

    added index

---
+++ app/index.php
@@ -0,0 +1,22 @@
+<?php
+	// Start the session, to get the data
+	session_start();
+
+	// If the user is logged in redirect to the app page
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+
+        // Redirect to app
+        header('Location: ../app.php');
+
+        // Exit script
+        exit();
+        
+    } else {
+
+        // Redirect to login
+        header('Location: ../account/index.html');
+
+        // Exit script
+        exit();
+    }
+?>
