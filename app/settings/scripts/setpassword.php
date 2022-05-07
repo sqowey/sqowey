b0ced88f00315f2db3725dfd5ee527e3678bf990
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-05-08 00:11:32 +0200

    removed unneeded

--- app/settings/scripts/setpassword.php
+++ app/settings/scripts/setpassword.php
@@ -4,7 +4,6 @@
     $new_pw = $_POST['new_pw'];
     $new_pw_repeat = $_POST['new_pw_repeat'];
 
-
 	// Start the session, to get the data
 	session_start();
 
@@ -13,55 +12,5 @@
 
     // Try to Connect with credentials
     $con = mysqli_connect($db_host, $db_user, $db_pass, 'accounts');
-
-    // Prepare the SQL
-    if ($stmt = $con->prepare('SELECT password FROM accounts WHERE username = ?')) {
-
-        // Bind the parameters
-        $stmt->bind_param('s', $_SESSION['name']);
-
-        // Execute the statement
-        $stmt->execute();
-
-        // Bind the result
-        $stmt->bind_result($db_pw);
-
-        // Fetch the result
-        $stmt->fetch();
-
-        // Close the statement
-        $stmt->close();
-
-        // Check if the old password is correct
-        if (password_verify($old_pw, $db_pw)) {
-
-            // Check if the new password is the same as the repeat
-            if ($new_pw == $new_pw_repeat) {
-
-                // Prepare the SQL
-                if ($stmt = $con->prepare('UPDATE accounts SET password = ? WHERE username = ?')) {
-
-                    // Bind the parameters
-                    $stmt->bind_param('ss', password_hash($new_pw, PASSWORD_DEFAULT), $_SESSION['name']);
-
-                    // Execute the statement
-                    $stmt->execute();
-
-                    // Close the statement
-                    $stmt->close();
-
-                    // Close the connection
-                    $con->close();
-                    
-                    // Logout the user
-                    session_destroy();
-
-                    // Redirect to the login page
-                    header('Location: ../../login.html');
-                }
-            }
-        }
-    }
-
 
 ?>
