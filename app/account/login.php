3a108ef4ad23c0ae482120d048ab38cf6ace6519
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-05-08 00:01:52 +0200

    added login script

--- app/account/login.php
+++ app/account/login.php
@@ -1,70 +1,60 @@
-<!-- This is the File used to authenticate the user at login -->
-
 <?php
 
     // Start the PHP_session
     session_start();
-    
-    // Get the database login-credentials
-    require("../config.php");
-    
-    // Try to Connect with credentials
+
+    // Variables
+    $db_config = require('../config.php');
+
+    // Get input
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Conect to database
     $con = mysqli_connect($db_host, $db_user, $db_pass, 'accounts');
+    if (mysqli_connect_errno()) {
+        header("Location: register.html?c=98");
+        exit;
+    } else {
 
-    // If there is an error whilst connecting, stop and display it
-    if ( mysqli_connect_errno() ) {
-        header('Location: login.html?c=01');
-        exit();
-    }
-
-    // Look, if there are inputs in the fields
-    if ( !isset($_POST['username'], $_POST['password']) ) {
-        header('Location: login.html?c=02');
-        exit();
-    }
-
-    // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-    if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-
-        // Bind parameters (s = string, i = int, b = blob, etc)
-        $stmt->bind_param('s', $_POST['username']);
+        // Check if account exists
+        $stmt = $con->prepare("SELECT id FROM accounts WHERE username = ?");
+        $stmt->bind_param('s', $username);
         $stmt->execute();
-
-        // Store the result so we can check if the account exists in the database.
         $stmt->store_result();
-
-        // Check if Account exist
-        if ($stmt->num_rows > 0) {
-            $stmt->bind_result($id, $password);
-            $stmt->fetch();
-
-            // Account exists, now we verify the password.
-            // Note: remember to use password_hash in your registration file to store the hashed passwords.
-            if (password_verify($_POST['password'], $password)) {
-                
-                // Verification success! User has logged-in!
-                // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-                session_regenerate_id();
-                $_SESSION['loggedin'] = TRUE;
-                $_SESSION['name'] = $_POST['username'];
-                $_SESSION['id'] = $id;
-                
-                // Redirect to the app
-                header('Location: ../app.php');
-            } else {
-
-                // Incorrect password
-                header('Location: login.html?c=03');
-                exit();
-            }
+        if ($stmt->num_rows == 0) {
+            header("Location: register.html?c=14");
+            exit;
         } else {
 
-            // Incorrect username
-            header('Location: login.html?c=03');
-            exit();
-        }
+            // Get id, salt and password hash from database
+            $stmt = $con->prepare("SELECT id, salt, password_hash, email, account_version FROM accounts WHERE username = ?");
+            $stmt->bind_param('s', $username);
+            $stmt->execute();
+            $stmt->store_result();
+            $stmt->bind_result($id, $salt, $password_hash, $email, $account_version);
+            $stmt->fetch();
 
-        // Close the mysql connection
-        $stmt->close();
+            // Add salt and password and check if right
+            if($account_version < 2) {
+                $pw_with_hash = $password;
+            } else {
+                $pw_with_hash = $salt . $password;
+            }
+            if (!password_verify($pw_with_hash, $password_hash)) {
+                header("Location: register.html?c=14");
+                exit;
+            } else {
+
+                // Set session variables
+                $_SESSION['id'] = $id;
+                $_SESSION['name'] = $username;
+                $_SESSION['email'] = $email;
+
+                // Redirect to app
+                header("Location: ../app.php");
+
+            }
+        }
     }
 ?>
