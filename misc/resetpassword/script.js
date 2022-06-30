27dfffbdc50d09a5337e3ae34ae552fa15b6c374
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-06-30 14:57:33 +0200

    added username takeover

--- misc/resetpassword/script.js
+++ misc/resetpassword/script.js
@@ -4,3 +4,10 @@ function openCodeInput() {
     location.assign("two.html?username=" + username + "&mail=" + mail);
 }
 
+// Check if url contains "username" and "mail"
+if (location.href.indexOf("?username=") > -1 && location.href.indexOf("&mail=") > -1) {
+    var username = location.href.split("two.html?username=")[1].split("&mail=")[0];
+    var mail = location.href.split("&mail=")[1];
+    document.getElementById("username").value = username;
+    document.getElementById("mail").value = mail;
+}
