a9c2a5b79b2082b70bc254d0ebc767415720e924
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-07-01 18:23:39 +0200

    added new id generator

--- app/account/register.php
+++ app/account/register.php
@@ -1,4 +1,22 @@
 <?php
+    // Functions to generate an ID
+    function generateIDsection($length){
+        $id_charset = "abcdefghijklmnopqrstuvwxyz123465789";
+        $generated = "";
+        for ($i=0; $i < $length; $i++) { 
+            $new_char = substr($id_charset, mt_rand(0, strlen($id_charset)),1);
+            $generated .= $new_char;
+        }
+        return $generated;
+    }
+    function generateID(){
+        $gen_id = generateIDsection(8) . "-";
+        $gen_id .= generateIDsection(4) . "-";
+        $gen_id .= generateIDsection(4) . "-";
+        $gen_id .= generateIDsection(4) . "-";
+        $gen_id .= generateIDsection(12);
+        return $gen_id;
+    }
 
     // Start the PHP_session
     session_start();
@@ -68,6 +86,21 @@
                                 header("Location: register.html?c=12");
                                 exit;
                             } else {
+
+                                // Check if id is already taken
+                                $checking_id_double = true;
+                                while($checking_id_double){
+                                    $new_id = generateID();
+                                    $stmt = $con->prepare("SELECT * FROM accounts WHERE username = ?");
+                                    $stmt->bind_param('s', $username);
+                                    $stmt->execute();
+                                    $result = $stmt->get_result();
+                                    if ($result->num_rows > 0) {
+                                        // Do nothing (this will repeat the process) 
+                                    } else { 
+                                        $checking_id_double = false;
+                                    }
+                                }
                                 
                                 // Create a salt
                                 $salt = bin2hex(openssl_random_pseudo_bytes(32));
@@ -77,8 +110,8 @@
                                 $hashed_pw = password_hash($pw_with_salt, PASSWORD_DEFAULT);
 
                                 // Insert the user into the database
-                                $stmt = $con->prepare("INSERT INTO accounts (username, displayname, email, password, salt, account_version) VALUES (?, ?, ?, ?, ?, ?)");
-                                $stmt->bind_param('sssssi', $username, $displayname, $email, $hashed_pw, $salt, $current_account_version);
+                                $stmt = $con->prepare("INSERT INTO accounts (id, username, displayname, email, password, salt, account_version) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                                $stmt->bind_param('isssssi', $new_id, $username, $displayname, $email, $hashed_pw, $salt, $current_account_version);
                                 $stmt->execute();
 
                                 // Close the connection
