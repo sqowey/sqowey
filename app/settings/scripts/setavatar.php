9ec47ab51c3dd9bd27405b21a8ea50a391b79ba9
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-07-30 19:35:05 +0200

    added new uploading process

--- app/settings/scripts/setavatar.php
+++ app/settings/scripts/setavatar.php
@@ -1,45 +1,85 @@
 <?php 
     // Start the PHP_session
     session_start();
-    
+
     // get the user id
     $id = $_SESSION['id'];
 
     // The directory the avatars get uploaded to
     $upload_dir = "../../../files/avatars/";
-    $upload_path = $upload_dir.$id.".".strtolower(pathinfo($_FILES["avatar_upload"]["name"],PATHINFO_EXTENSION));
-    $imageFileType = strtolower(pathinfo($upload_path,PATHINFO_EXTENSION));
-    
-    if(isset($_FILES["avatar_upload"])) {
-        $imagesize = getimagesize($_FILES["avatar_upload"]["tmp_name"]);
 
-        if ($imagesize == false) {
-            header("Location: ../../settings/settings.php?e=filetype");
-        } else {
+    // Check if the server has received files
+    if ($_SERVER ["REQUEST_METHOD"] === "POST") {
 
-            // Check file size
-            if ($_FILES["avatar_upload"]["size"] > 800000) {
-                header("Location: ../../settings/settings.php?e=filesize");
-            } else {
+        // check if there are files
+        if (isset($_FILES['avatar_upload'])) {
 
-                // Allow certain file formats
-                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-                    header("Location: ../../settings/settings.php?e=filetype");
-                } else {
+            // create empty error list
+            $errors = [];
 
-                    if (move_uploaded_file($_FILES["avatar_upload"]["tmp_name"], $upload_path)) {
-                        header("Location: ../../settings/settings.php?m=success");
-                    } else {
-                        exit("Error".$upload_path);
-                        header("Location: ../../settings/settings.php?e=couldntupload");
-                    }
-                }
+            // List of allowed extensions
+            $extensions = ["bmp", "gif", "jpg", "jpeg", "png", "webp"];
+
+            // Get the file name
+            $file_name = $_FILES['avatar_upload']['name'];
+
+            // Get the temp file
+            $file_tmp = $_FILES['avatar_upload']['tmp_name'];
+
+            // Get the file type
+            $file_type = $_FILES['avatar_upload']['type'];
+
+            // Get the file size
+            $file_size = $_FILES['avatar_upload']['size'];
+
+            // Get the file extension
+            $explode_var = explode('.', $file_name);
+            $file_ext = strtolower(end($explode_var));
+
+            // create the new file 
+            $file = $upload_dir . str_replace("-", "", $id) . ".png";
+
+            // check if file extension is allowed
+            if (!in_array($file_ext, $extensions)) {
+
+                // Create error record
+                $errors[] = 'Dateityp nicht erlaubt: ' . $file_name . ' ' . $file_ext;
             }
+
+            // check if file size is too big
+            if ($file_size > 2097152) {
+
+                // Create error record
+                $errors[] = 'Datei zu groß: ' . $file_name . ' ' . $file_type;
+            }
+            
+            // If there are no errors, move the file
+            if (empty($errors)) {
+
+                // Convert images to png
+                switch ($file_ext) {
+                    case 'bmp':
+                        $image = imagecreatefrombmp($file_tmp);
+                        break;
+                    case 'jpg':
+                    case 'jpeg':
+                        $image = imagecreatefromjpeg($file_tmp);
+                        break;
+                    case 'gif':
+                        $image = imagecreatefromgif($file_tmp);
+                        break;
+                    case 'png':
+                        $image = imagecreatefrompng($file_tmp);
+                        break;
+                    case 'webp':
+                        $image = imagecreatefromwebp($file_tmp);
+                        break;
+                }
+                imagepng($image, $file);
+            }
+            
+            // If there are errors, print them
+            if ($errors) print_r($errors);
         }
-    
-    } else {
-
-        header("Location: ../../settings/settings.php?m=error");
     }
-
 ?>
