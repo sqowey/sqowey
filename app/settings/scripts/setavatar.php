85e93ebb6120ebdf9fb80526268314fd9560b039
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-07-30 20:10:17 +0200

    added image square cropping

--- app/settings/scripts/setavatar.php
+++ app/settings/scripts/setavatar.php
@@ -78,6 +78,42 @@
                         $image = imagecreatefromwebp($file_tmp);
                         break;
                 }
+
+                // Crop the image to be squared
+                function cropAlign($image, $cropWidth, $cropHeight, $horizontalAlign = 'center', $verticalAlign = 'middle') {
+                    $width = imagesx($image);
+                    $height = imagesy($image);
+                    $horizontalAlignPixels = calculatePixelsForAlign($width, $cropWidth, $horizontalAlign);
+                    $verticalAlignPixels = calculatePixelsForAlign($height, $cropHeight, $verticalAlign);
+                    return imageCrop($image, [
+                        'x' => $horizontalAlignPixels[0],
+                        'y' => $verticalAlignPixels[0],
+                        'width' => $horizontalAlignPixels[1],
+                        'height' => $verticalAlignPixels[1]
+                    ]);
+                }
+                function calculatePixelsForAlign($imageSize, $cropSize, $align) {
+                    switch ($align) {
+                        case 'left':
+                        case 'top':
+                            return [0, min($cropSize, $imageSize)];
+                        case 'right':
+                        case 'bottom':
+                            return [max(0, $imageSize - $cropSize), min($cropSize, $imageSize)];
+                        case 'center':
+                        case 'middle':
+                            return [
+                                max(0, floor(($imageSize / 2) - ($cropSize / 2))),
+                                min($cropSize, $imageSize),
+                            ];
+                        default: return [0, $imageSize];
+                    }
+                }
+                $image = cropAlign($image, min(imagesx($image), imagesy($image)), min(imagesx($image), imagesy($image)));
+
+    
+
+                // Save the image
                 imagepng($image, $file);
             }
             
